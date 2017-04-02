539a4c21cef30d1c2413bc2b27fb961118996959
JeroenCops <jernncops@live.be>
2017-04-02 19:29:51 +0200

    State of sidenav is kept in a sessionStorage

--- src/js/jquery.sidenav.js
+++ src/js/jquery.sidenav.js
@@ -6,19 +6,20 @@ $( "#toggle-menu" ).click(function() {
 // Sets the selected menu item active.
 $(document).ready(function(){
   var activeTab = sessionStorage.getItem('activeTab');
+  console.log("Active: " + activeTab);
 
   if (activeTab) {
-    $('.tab-content').removeClass('show');
+    $('.post-content').removeClass('show');
     $(activeTab).addClass('show');
   }
 
-  $('.side-nav a').click(function(e) {
+  $('.side-nav > ul > li > a').click(function(e) {
     e.preventDefault();
     sessionStorage.setItem('activeTab', $(this).attr('href'));
     activeTab = sessionStorage.getItem('activeTab');
 
-    $('.tab-content').removeClass('show');
-		$('.side-nav li').removeClass('active');
+    $('.post-content').removeClass('show');
+		$('.side-nav > ul > li').removeClass('active');
 
     $(activeTab).addClass('show');
     $(this).parents('li').addClass('active');
