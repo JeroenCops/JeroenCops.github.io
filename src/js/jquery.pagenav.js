ccd0a97d645c37072e6b1b1012c124978f79ef1b
JeroenCops <jernncops@live.be>
2017-04-02 13:45:44 +0200

    Refactored navigation and updated the error page

--- src/js/jquery.pagenav.js
+++ src/js/jquery.pagenav.js
@@ -1,8 +1,12 @@
+// Sets the selected menu item active.
 $(document).ready(function(){
   $('.page-nav a').click(function(e) {
     e.preventDefault();
+
+    $('.tab-content').removeClass('show');
 		$('.page-nav li').removeClass('active');
+
+    $($(this).attr('href')).addClass('show');
     $(this).parents('li').addClass('active');
-    moveMarker();
   });
 });
--- src/js/jquery.sidenav.js
+++ src/js/jquery.sidenav.js
@@ -1,2 +1,26 @@
 // Toggles the side navigation when the window resizes or a button is pressed.
-$( "sidenav" )
+$( "#toggle-menu" ).click(function() {
+
+});
+
+// Sets the selected menu item active.
+$(document).ready(function(){
+  var activeTab = sessionStorage.getItem('activeTab');
+
+  if (activeTab) {
+    $('.tab-content').removeClass('show');
+    $(activeTab).addClass('show');
+  }
+
+  $('.side-nav a').click(function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('activeTab', $(this).attr('href'));
+    activeTab = sessionStorage.getItem('activeTab');
+
+    $('.tab-content').removeClass('show');
+		$('.side-nav li').removeClass('active');
+
+    $(activeTab).addClass('show');
+    $(this).parents('li').addClass('active');
+  });
+});
